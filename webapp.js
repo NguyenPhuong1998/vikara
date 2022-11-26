784eef4094cb11a39cd4d30d4b80f75a82393847
Phạm Nguyên Phương <phamnguyenphuong@hotmail.com>
2022-11-26 11:05:54 +0700

    Tối ưu lại giao diện

--- webapp.js
+++ webapp.js
@@ -51,15 +51,12 @@ function onPlayerStateChange(event) {
     case YT.PlayerState.ENDED:
       nextVideo();
       break;
-
     case YT.PlayerState.PLAYING:
       isPlay = true;
       break;
-
     case YT.PlayerState.PAUSED:
       isPlay = false;
       break;
-
     default:
       break;
   }
@@ -69,13 +66,10 @@ function controlVideo(eventName) {
     case "Replay":
       console.log("Replay");
       break;
-
+      
     case "Pause":
-      if (isPlay) {
-        player.pauseVideo();
-      } else {
-        player.playVideo();
-      }
+      if (isPlay) player.pauseVideo();
+      else player.playVideo();
       break;
 
     case "Next":
@@ -83,11 +77,8 @@ function controlVideo(eventName) {
       break;
 
     case "Mute":
-      if (player.isMuted()) {
-        player.unMute();
-      } else {
-        player.mute();
-      }
+      if (player.isMuted()) player.unMute();
+      else  player.mute();
       break;
 
     default:
@@ -126,28 +117,20 @@ function searchYoutube() {
 
   fetch(
     "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&type=video&key=AIzaSyBvoC3r9qzkrrX-nTcUd6Z1xONs4X5fdnU&q=" +
-      textSearch,
+    textSearch,
     requestOptions
   )
     .then((response) => response.text())
     .then((rawResult) => {
-      const result = JSON.parse(rawResult);
-
       searchVideos = [];
-      result["items"].forEach((video) => {
-        var videoId = video["id"]["videoId"];
-        var title = video["snippet"]["title"];
-        var channelTitle = video["snippet"]["channelTitle"];
-        var image = video["snippet"]["thumbnails"]["default"]["url"];
+      JSON.parse(rawResult)["items"].forEach((video) => {
         searchVideos.push({
-          videoId: videoId,
-          title: title,
-          channelTitle: channelTitle,
-          image: image
+          videoId: video["id"]["videoId"],
+          title: video["snippet"]["title"],
+          channelTitle: video["snippet"]["channelTitle"],
+          image: video["snippet"]["thumbnails"]["default"]["url"]
         });
       });
-
-      console.log(searchVideos);
       updateContentSearch();
     })
     .catch((error) => console.log("error", error));
@@ -173,20 +156,16 @@ function updateContentSearch() {
   myNode.innerHTML = text;
 
   function myFunction(index, value) {
-    text +=
-      '<div onclick="addVideo(' +
-      index +
-      ')"><img src="' +
-      value["image"] +
-      '"/><h3>' +
-      value["title"] +
-      "</h3><p>" +
-      value["channelTitle"] +
-      "</p></div>\n";
+    text += `<div onclick="addVideo(${index})">
+        <img src="${value["image"]}"/>
+        <h3>${value["title"]}</h3>
+        <p>${value["channelTitle"]}</p>
+      </div>\n`;
   }
 }
 
 function updateContentNext() {
+  console.log(nextVideos)
   var myNode = document.getElementById("contentNext");
 
   // Xóa các phần tử cũ
@@ -202,7 +181,7 @@ function updateContentNext() {
 
   function myFunction(index, value) {
     if (value === undefined) return;
-    text += `<div class="nextItem" name="next_${index}" onclick="test_menu()">
+    text += `<div class="nextItem" onclick="viewNavigationMenu(${index})">
         <img src="${value["image"]}"/>
         <h3>${value["title"]}</h3>
         <p>${value["channelTitle"]}</p>
@@ -210,70 +189,79 @@ function updateContentNext() {
   }
 }
 
-var abc = false;
+var isHidenNavigationMenu = false;
 
-// this is from another SO post...
 $(document).bind("click", function (event) {
-  console.log("click", abc);
-  if (abc) document.getElementById("rmenu").className = "hide";
-  abc = true;
-  // var hide_rmenu = true;
-  // console.log(event.currentTarget.nodeName);
-  // if (event.currentTarget.nodeName === "DIV") hide_rmenu = false;
-  // if (hide_rmenu === true) document.getElementById("rmenu").className = "hide";
+  if (isHidenNavigationMenu) document.getElementById("rmenu").className = "hide";
+  isHidenNavigationMenu = true;
 });
 
-function test_menu() {
+function viewNavigationMenu(index) {
   function mouseX(evt) {
-    if (evt.pageX) {
-      return evt.pageX;
-    } else if (evt.clientX) {
-      return (
-        evt.clientX +
-        (document.documentElement.scrollLeft
-          ? document.documentElement.scrollLeft
-          : document.body.scrollLeft)
-      );
-    } else {
-      return null;
-    }
+    if (evt.pageX) return evt.pageX;
+    else if (evt.clientX) return (evt.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft));
+    else return null;
   }
 
   function mouseY(evt) {
-    if (evt.pageY) {
-      return evt.pageY;
-    } else if (evt.clientY) {
-      return (
-        evt.clientY +
-        (document.documentElement.scrollTop
-          ? document.documentElement.scrollTop
-          : document.body.scrollTop)
-      );
-    } else {
-      return null;
-    }
+    if (evt.pageY) return evt.pageY;
+    else if (evt.clientY) return (evt.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop));
+    else return null;
   }
 
-  var temp_x = mouseX(event);
+  var temp_x = mouseX(window.event);
   var body_x = document.getElementsByTagName("body")[0].offsetWidth;
   temp_x = body_x - temp_x < 113 ? body_x - 113 : temp_x;
 
   document.getElementById("rmenu").className = "show";
-  document.getElementById("rmenu").style.top = mouseY(event) + "px";
+  document.getElementById("rmenu").style.top = mouseY(window.event) + "px";
   document.getElementById("rmenu").style.left = temp_x + "px";
 
-  var index = event.currentTarget.attributes.name.value.split("_")[1];
+  // var index = event.currentTarget.attributes.name.value.split("_")[1];
   document.getElementById("rmenu_top").onclick = function () {
+    console.log("Top");
     nextVideos.unshift(nextVideos.splice(index, 1)[0]);
     updateContentNext();
   };
   document.getElementById("rmenu_delete").onclick = function () {
+    console.log("Delete");
     nextVideos.splice(index, 1);
     updateContentNext();
   };
 
-  abc = false;
-  console.log("test_menu", abc);
-
-  window.event.returnValue = false;
+  isHidenNavigationMenu = false;
 }
+
+nextVideos = [
+  {
+    "videoId": "qmD5zW1lfC0",
+    "title": "Bài hát ABC cho trẻ  Học tiếng Anh bằng ABC Song",
+    "channelTitle": "DreGGor",
+    "image": "https://i.ytimg.com/vi/qmD5zW1lfC0/default.jpg"
+  },
+  {
+    "videoId": "yqsux6Y1D1M",
+    "title": "ABC Song - Bài hát ABC Tiếng Việt [ Full ] | Giúp Bé Học Chữ Cái Qua Bài hát | VOI TV",
+    "channelTitle": "VOI TV",
+    "image": "https://i.ytimg.com/vi/yqsux6Y1D1M/default.jpg"
+  },
+  {
+    "videoId": "xY3Z8acE8ew",
+    "title": "The ABC Song | CoComelon Nursery Rhymes &amp; Kids Songs",
+    "channelTitle": "Cocomelon - Nursery Rhymes",
+    "image": "https://i.ytimg.com/vi/xY3Z8acE8ew/default.jpg"
+  },
+  {
+    "videoId": "hq3yfQnllfQ",
+    "title": "Phonics Song with TWO Words - A For Apple - ABC Alphabet Songs with Sounds for Children",
+    "channelTitle": "ChuChu TV Nursery Rhymes & Kids Songs",
+    "image": "https://i.ytimg.com/vi/hq3yfQnllfQ/default.jpg"
+  },
+  {
+    "videoId": "GgXlqcklJ7Y",
+    "title": "Çfare fitoi Irma Libohova nga Kenga Magjike? Ne &#39;Abc e pasdites&#39; diva: A do duhet ze per te kenduar?",
+    "channelTitle": "ABC News Albania",
+    "image": "https://i.ytimg.com/vi/GgXlqcklJ7Y/default.jpg"
+  }
+]
+updateContentNext();
