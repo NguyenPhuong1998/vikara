5f5849d799e3eb3f6d25821e591baca8df5b18f0
phuongpn2 <VSS_pnp@1808>
2022-12-16 16:25:11 +0700

    Test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/*', function(req, res) {
     }
     
     if (url.endsWith(".html") || url.endsWith(".css") || url.endsWith(".js")) {
-        res.sendFile(path.join(__dirname, 'views' + url));
+        res.sendFile(path.join(__dirname, '/views' + url));
     }
 });
 
