0e4409fd41f6b05f52a597bf46182a270c22e0ae
phuongpn2 <VSS_pnp@1808>
2022-12-16 14:14:24 +0700

    Sử dụng thư viện ngoài

--- api/product.js
+++ api/product.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * GET product list.
+ *
+ * @return product list | empty.
+ */
 router.get("/", async (req, res) => {
   try {
     res.json({
       status: 200,
-      message: "Get data has successfully.",
+      message: "Get data has successfully",
     });
   } catch (error) {
     console.error(error);
