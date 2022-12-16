72573ef111955f0c0ff4ec4cc5ced4a49fc53e43
phuongpn2 <VSS_pnp@1808>
2022-12-16 14:41:11 +0700

    Thêm fontend

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
 
-app.use("/webapp", express.static("webapp"))
+// app.use("/", express.static("webapp"))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'webapp/index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
