ca3e98f2f1191ffb5a92eca3f9e76dee89fead6c
phuongpn2 <VSS_pnp@1808>
2022-12-16 17:05:02 +0700

    Test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const product = require("./api/product");
 const path = require('path');
+var fs = require('fs');
 
 app.use(express.json({ extended: false }));
 
@@ -10,6 +11,7 @@ app.use("/api/product", product);
 app.get('/*', function(req, res) {
     var url = req.url.toString();
     console.log(url);
+    console.log(fs.readdirSync(path.join(__dirname, 'views')))
 
     if (url == "/") {
         url = "/index.html";
